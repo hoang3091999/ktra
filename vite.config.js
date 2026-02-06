e343b9c7fcb3bb8eb8d2006a2349f49bf12d3db7
HoangleRMIT <s3715120@rmit.edu.vn>
2026-02-06 18:42:55 +0700

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://hoang3091999.github.io/ktra/",
+  base: "/ktra/",
   plugins: [react()],
 })
